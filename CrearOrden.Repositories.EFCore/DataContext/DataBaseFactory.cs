cc43d1be03f0bdfbe6db6d49acc0249123f2c1fd
Sergi Ortiz Gómez <drualcman@msn.com>
2021-09-18 10:18:48 +0800

    Add project files.

---
+++ CrearOrden.Repositories.EFCore/DataContext/DataBaseFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrearOrden.Repositories.EFCore.DataContext
+{
+    /// <summary>
+    /// Esta clase se usa solo en tiempo de diseño para poder crear la base de datos dierectamente
+    /// </summary>
+    class DataBaseFactory : IDesignTimeDbContextFactory<DataBaseContext>
+    {
+        public DataBaseContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<DataBaseContext> optionBuilder = new DbContextOptionsBuilder<DataBaseContext>();
+            optionBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;database=CrearOrdenDb");
+            return new DataBaseContext(optionBuilder.Options);
+        }
+    }
+}
